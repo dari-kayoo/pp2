604e36765aad2ca6007fff1848d6eeb04472540c
dari-kayoo <78098445+dari-kayoo@users.noreply.github.com>
2021-01-29 22:23:41 +0600

    new examples

---
+++ 10.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first value:"))
+b = int(input("Enter the second value:"))
+if a > b:
+    print(a, "is greater than ", b)
+else :
+    print(a, "is not greater than ", b)
---
+++ 8.py
@@ -0,0 +1,16 @@
+print("hello")
+print('world')
+a = """I can write 
+a lot of words
+in each line, then print it.
+"""
+#or i can write it with single quotes
+print("can" in a)
+print("lolipop" not in a)
+print(a)
+d = "good"
+print(len(d))
+print(a[6])
+
+for i in "weare":
+    print(i)
