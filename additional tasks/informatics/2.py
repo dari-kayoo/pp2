3662dbcba50841f4f0e919a175ad3b2d6c87e61a
dari-kayoo <78098445+dari-kayoo@users.noreply.github.com>
2021-02-16 10:52:29 +0600

    Add files via upload

---
+++ additional tasks/informatics/2.py
@@ -0,0 +1,6 @@
+n = 20
+fact = 1
+while n > 0:
+    fact *= n
+    n-=1
+print(fact)    
---
+++ additional tasks/informatics/5.py
@@ -0,0 +1,10 @@
+pi_list = list()
+pi1 = 1
+pi2 = 1
+for i in range (10):
+    pi3 = pi1/(pi2**2)
+    pi_list.append(pi3)
+    pi2+=1
+result = float(pi_list[0]+pi_list[1]+pi_list[2]+pi_list[3]+pi_list[4]+pi_list[5]+pi_list[6]+pi_list[7]+pi_list[8]+pi_list[9])    
+res = result*6
+print(res** .5)
