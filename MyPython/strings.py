798e02078eaf6d38d423a07b321c293c4a30212d
dari-kayoo <78098445+dari-kayoo@users.noreply.github.com>
2021-03-03 01:55:52 +0600

    Add files via upload

---
+++ MyPython/strings.py
@@ -0,0 +1,31 @@
+#indexing
+greeting = 'hello'
+print(len(greeting))
+print(greeting[0])
+print(greeting[3])
+print(greeting[-1])
+#i can use - value to take from back of the word
+
+#slicing
+print(greeting[2:])
+#from index two to the end of the word
+
+print(greeting[2:4])
+#from 2index to 4index
+
+print(greeting[-3: -1])
+#also like those but from back
+
+print(greeting[:3])
+print(greeting[:])
+#all symbols of word
+
+print(greeting[::2])
+#выскакиваем через одну букву
+
+print(greeting[1::2])
+#without 1st symbol
+
+print(greeting[2::2])
+print(greeting[::-1])
+#to reverse word
---
+++ MyPython/tuple.py
@@ -0,0 +1,7 @@
+#tuple is immutable
+tuple1 = 1, 2, 5 # you can write without brackets
+tuple2 = ('hi', 'hello', 'me')
+tuple3 = (3, 3.5, 85.47)
+print(tuple1)
+print(tuple2)
+print(tuple3)
