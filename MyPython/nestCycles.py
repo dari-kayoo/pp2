abcc6ac60f6a1a666cd441fb3c38e8067b59be04
dari-kayoo <78098445+dari-kayoo@users.noreply.github.com>
2021-03-07 18:03:19 +0600

    Add files via upload

---
+++ MyPython/nestCycles.py
@@ -0,0 +1,20 @@
+smile = '\U0001f600'
+# for i in range(5):
+#     count = 0
+#     emoticans = ''
+#     while count <= i:
+#         emoticans += smile
+#         count += 1
+
+#     print(emoticans)    
+
+
+# for i in range(5):
+#     emoticans = ''
+#     for count in range(i+1):
+#         emoticans+=smile
+
+#     print(emoticans)      
+
+for number in range(1, 11):
+    print(smile * number)
